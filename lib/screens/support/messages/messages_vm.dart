625085870183879aa78cf7ae0b2ffca47981c158
Omar <omm8233@gmail.com>
2023-02-26 12:53:44 +0200

    main functionality of gobid admin

--- lib/screens/support/messages/messages_vm.dart
+++ lib/screens/support/messages/messages_vm.dart
@@ -1,11 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gobid_admin/base.dart';
 import 'package:gobid_admin/model/chat.dart';
+import 'package:gobid_admin/model/message.dart';
 import 'package:gobid_admin/network/remote/db_utils_chat.dart';
 import 'package:gobid_admin/screens/support/messages/messages_navigator.dart';
 import 'package:gobid_admin/shared/strings/app_strings.dart';
-
-import '../../../../model/message.dart';
-import '../../../base.dart';
 
 class MessagesViewModel extends BaseViewModel<MessagesNavigator> {
   Stream<QuerySnapshot<Message>> getMessages(String userId) {
